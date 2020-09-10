1228f6273e8a1ed85882ff7bbbfca59c443a5da1
Jonathan Bernstein <jjbernstein@gmail.com>
2020-09-10 22:17:32 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -6,4 +6,13 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-# code your move method here!
+def input_to_index(user_input)
+  user_index = user_input.to_i
+  return user_index - 1
+end
+
+def move(board, position, character = "X")
+  board[position] = character
+  display_board(board)
+  return board
+end
